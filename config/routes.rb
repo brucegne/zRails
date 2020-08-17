a70e2243b0f1875890a03377d41c9850e048eb29
Glitch (rails-app) <none>
2020-08-17 09:44:52 +0000

    🏡🐽 Checkpoint ./config/routes.rb:2119616/26

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :blog_posts
   resources :titles
+  root 'blog_posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
